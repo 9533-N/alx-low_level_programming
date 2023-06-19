34e9b5c45449228f1916b4f7628e695dd10296fa
9533-N <nombuso.geba@gmail.com>
2023-06-19 13:56:51 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	printf("Size of a chat: %ln byte(s)\n", sizeof(char));
-	printf("Size of a int: %ln byte(s)\n", sizeof(int));
-	printf("Size of a long int: %ln byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %ln byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %ln byte(s)\n", sizeof(float));
+	printf("Size of a chat: %lu byte(s)\n", sizeof(char));
+	printf("Size of a int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
